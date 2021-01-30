f780e209b5572ca80599cd845a55461658558ae8
Dasha Lary <dasha4988@gmail.com>
2021-01-30 17:15:25 -0500

    fixed typo in provider

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 ReactDOM.render(
   <Provider store={store}>
      <App />
-  </Provider>
+  </Provider>,
   document.getElementById('root')
 );
 
