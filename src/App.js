250e80186dc5d4b0c4c8c21207880263141bf440
Dasha Lary <dasha4988@gmail.com>
2021-01-31 13:37:25 -0500

    imported getPosts action to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import PostForm from './containers/PostForm'
+import { getPosts } from './actions/posts'
 
 
 class App extends Component {
@@ -31,25 +32,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { getPosts })(App)
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default App;
