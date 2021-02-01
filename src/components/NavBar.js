e72b0402a843c0fcc9d0533831d1175f25bb0447
Dasha Lary <dasha4988@gmail.com>
2021-02-01 11:30:12 -0500

    created navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'blue',
+    textDecoration: 'none',
+    color: 'white',
+  }
+  
+  const NavBar = () => {
+    return (
+      <div className="navbar">
+        <NavLink to="/" exact style={link} activeStyle={{background: 'pink'}}>Home</NavLink>
+        <NavLink to="/posts" exact style={link} activeStyle={{background: 'pink'}}>Posts</NavLink>
+      </div>
+    );
+  };
+  
+  export default NavBar;
+  
