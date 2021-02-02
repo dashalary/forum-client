ec2afe3469d79bc5931eb2f776bc2feb4a8a5916
Dasha Lary <dasha4988@gmail.com>
2021-02-02 11:16:57 -0500

    added redirect on form submit

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const Post = ({ post }) => {
+const Post = (props) => {
+    let post = props.posts.filter(p => p.id == props.match.params.id)[0]
+    return (
         <div>
             <div className="card card-inverse card-success card-primary mb-3 text-center">
                 <div className="card-block">
                     <blockquote className="card-blockquote">
                     <p>{post.content}</p>
-                        <footer>- author <cite title="Source Title">{post.author}</cite></footer>
+                        <footer>by: <cite title="Source Title">{post.author}</cite></footer>
                     </blockquote>
                 </div>
             </div>
         </div>
+    )
 }
 
 export default Post
