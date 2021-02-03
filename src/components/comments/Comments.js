b4ba002ba7229dcc8de5c32f56d90014c6b99402
Dasha Lary <dasha4988@gmail.com>
2021-02-03 12:17:41 -0500

    added empty delete button

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -2,22 +2,12 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import { deleteComment } from '../../actions/posts'
 
-// const Comments = (props) => {
-//   let post = this.props.posts.filter(p => p.id == props.match.params.id)[0]
-//   return (
-//     <div>
-//       Comments:
-//       {post.comments.map(comment =><li key={comment.id}>{comment.content} - {comment.author}</li>)}
-//     </div>
-//   )
-// } 
+
 class Comments extends Component {
 
-  // state = {}
-
-//     // handleDelete = (comment) => {
-//     //    this.props.deleteComment(comment.id, comment.post_id)
-//     // }
+    handleDelete = (comment) => {
+      this.props.deleteComment(comment.id, comment.post_id)
+    }
    
 //     // like = (id) => {
 //     //  this.setState({id ? id: id += 1 : id : 1})
@@ -27,25 +17,28 @@ class Comments extends Component {
     //  const comment = this.props.comments.select(comment => comment.id === commentId)
     //  const post = comments.post.select(post => post.id === postId)
     //   {/* <button onClick={() => this.like(c.id)}>Like Comment {this.state[c.id] ? this.state[c.id] : 0}</button><button onClick={() => this.handleDelete(c)}>Delete Comment</button></li> */}
-
+    
      return (
          <div>
+           <hr />
+           <p>Comments:</p>
+           
           {this.props.comments && this.props.comments.map(comment =>
-           <li key={comment.id}>{comment.content} - {comment.author}</li>
+           <li key={comment.id}>{comment.content} - says {comment.author}
+           <br></br>
+           <button onClick={() => this.handleDelete(comment)}>Delete Comment</button>
+            </li>
           )}
-          <hr />
-          <p>Comments:</p>
          </div>
     )
   }
 }
 
-
     const mapStateToProps = state => {
       return {
-        comments: state.postReducer.comments 
+        comments: state.postReducer.comments,
+        posts: state.postReducer.posts
       }
     }
 
    export default connect(mapStateToProps)(Comments)
-  //  connect(null, {deleteComment})(Comments)
